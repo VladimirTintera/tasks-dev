9ae953715bd4ab979d18876f5d8b29c507b1e2b8
Vladimír Tintěra <vladimir.tintera@magicware.cz>
2026-04-22 07:14:00 +0200

    guard upgrade

--- engine/src/commonMain/kotlin/eu/tintera/tasks/core/preconditions/NetworkStateTaskPrecondition.kt
+++ engine/src/commonMain/kotlin/eu/tintera/tasks/core/preconditions/NetworkStateTaskPrecondition.kt
@@ -1,9 +1,7 @@
 package eu.tintera.tasks.core.preconditions
 
-import eu.tintera.tasks.EventBus
 import eu.tintera.tasks.core.NetworkState
 import eu.tintera.tasks.core.data.ProcessableTask
-import eu.tintera.tasks.core.data.Task
 import kotlinx.coroutines.flow.map
 
 internal class NetworkStateTaskPrecondition(
@@ -12,7 +10,6 @@ internal class NetworkStateTaskPrecondition(
     override fun hasConstraint(task: ProcessableTask) = task.networkRequired
 
     override fun isValid(task: ProcessableTask) = networkState.state().map {
-        EventBus.send("NetworkStateTaskPrecondition", "state = $it")
         when (it) {
             NetworkState.State.Disconnected -> PreconditionResult.Unmet
             NetworkState.State.Connected -> PreconditionResult.Met
